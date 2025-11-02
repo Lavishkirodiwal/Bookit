b27b4a153cfd2a03ef7224a25c577c4837dd305a
Lavishkirodiwal <160909416+Lavishkirodiwal@users.noreply.github.com>
2025-11-02 14:05:13 +0530

    Update booking.js

--- backend/routes/booking.js
+++ backend/routes/booking.js
@@ -4,5 +4,7 @@ import { createBooking } from "../controller/places.js";
 const router = express.Router();
 
 router.post("/", createBooking);
+router.get("/:id", getBookingById); 
 
 export default router;
+
